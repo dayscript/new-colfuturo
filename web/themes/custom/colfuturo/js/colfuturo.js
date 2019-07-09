b6e5f86d2b3be35934d85ca0dffed6d504e9737a
JoseP <joseft40@gmail.com>
2019-07-09 14:12:35 -0400

    fix in home

--- web/themes/custom/colfuturo/js/colfuturo.js
+++ web/themes/custom/colfuturo/js/colfuturo.js
@@ -11,6 +11,23 @@
   Drupal.behaviors.exampleBehavior = {
     attach: function (context, settings) {
       //alert("I'm alive!");
+      $('.block-views-blockgestion-en-cifras-block-1 .field-value').each(function() {
+        var porcenta = $(this).text();
+        var lastChar = porcenta[porcenta.length -1];
+        var restoChar = porcenta.substring(0, porcenta.length - 1);;
+
+        if (lastChar == '%') {
+          $(this).html(restoChar + '<b class="porcenta">' + lastChar + '</b>');
+        }
+      });
+
+      if ($(window).width() < 480 || $(window).height() < 480) {
+        $('.block-views-block-asesoria-home-block-1 .views-field-nothing .columns:last-child').each(function () {
+          $(this).insertBefore($(this).prev());
+        });
+
+        $("#block-colfuturo-branding img").attr("src", "http://localhost/drupal8/newcolf/new-colfuturo/web/sites/default/files/logo_blanco.png");
+      }
     }
   };
 
