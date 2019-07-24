fc3e3b6f344822b76c1f9a8777c6919f16256c74
Soporte Colombia Link <soporte@linkdigital.co>
2019-07-24 00:45:00 +0000

    submenu-fixed

--- web/themes/custom/colfuturo/js/colfuturo.js
+++ web/themes/custom/colfuturo/js/colfuturo.js
@@ -11,6 +11,14 @@
   Drupal.behaviors.exampleBehavior = {
     attach: function (context, settings) {
       //alert("I'm alive!");
+		jQuery(window).scroll(function() {
+			console.log(jQuery(window).scrollTop());
+		    if (jQuery(window).scrollTop() > 706) {
+					jQuery("#sidebar-first").addClass("submenu-fixed");
+		    } else {
+					jQuery("#sidebar-first").removeClass("submenu-fixed");
+		    }
+		});
     }
   };
 
