c67cf2b77b64cfce32c8d4c0b317921912cddf7c
cvillamil <cvillamil@dayscript.com>
2019-09-16 09:41:15 -0500

    menu side

--- web/themes/custom/colfuturo/js/colfuturo.js
+++ web/themes/custom/colfuturo/js/colfuturo.js
@@ -23,8 +23,6 @@
         jQuery("#block-homemenu .menu .estudios-exterior").parent().addClass("is-active");
         jQuery("#block-homemenu .menu .estudios-exterior").addClass("is-active");
       }
-
-      //alert("I'm alive!");
       if (Foundation.MediaQuery.is('small only')) {
         jQuery(window).scroll(function() {
           if (jQuery(window).scrollTop() > 37) {
@@ -32,7 +30,6 @@
           }else{
             jQuery(".header-page").removeClass("header-fixed");
           }
-          //console.log(jQuery(window).scrollTop());
           if (jQuery(window).scrollTop() >= 392) {
             jQuery("#sidebar-first nav").addClass("second-sticky");
           }else{
@@ -49,12 +46,6 @@
       		}else{
       			jQuery(".header-page").removeClass("header-fixed");
       		}
-/*		    if (jQuery(window).scrollTop() > 531) {
-          console.log(jQuery(window).scrollTop());
-					jQuery("#sidebar-first").addClass("submenu-fixed");
-		    } else {
-					jQuery("#sidebar-first").removeClass("submenu-fixed");
-		    }*/
       	});
       }
       jQuery('.submenu ul li ul li a').on('click', function (e) {
@@ -107,8 +98,6 @@ $(document).ready(function() {
     * scrollLeft .submenumobile-fixed
     **/
 });
-
-
 var thisArticle = $('main#main');
 var thisSidebar = $('#sidebar-first');
 function positionSidebar(){
@@ -117,50 +106,40 @@ function positionSidebar(){
 
     var windowScroll = $(window).scrollTop();
     var sweetSpot = ((articleHeight + articleFromTop) - thisSidebar.height());
-    console.log('ss: ' + sweetSpot);
-    console.log('current scroll: ' + windowScroll);
+    //console.log('ss: ' + sweetSpot);
+    //console.log('current scroll: ' + windowScroll);
     if ($(window).width() >= 1280) {
-        console.log('screen larger than 1280');
+        //console.log('screen larger than 1280');
         //if sidebar has hit the bottom
         if ((windowScroll + 40) >= sweetSpot) {
-            console.log('sidebar is at the bottom');
+            //console.log('sidebar is at the bottom');
             thisSidebar.css('position', 'relative');
-
-            console.log(sweetSpot);
-            console.log(thisSidebar.height());
-            //thisSidebar.css('top', (sweetSpot - (thisSidebar.height() + 60)));
             thisSidebar.css('top', (thisArticle.height() - thisSidebar.height()));
             thisSidebar.css('max-width', '433px');
             //Sidebar has not yet hit the bottom
         } else{
             //if scroll has not yet passed the top of the article - should still be absolute
             if ((windowScroll + 40) <= articleFromTop) {
-                console.log('Sidebar hasent passed top of article yet');
-                //jQuery("#sidebar-first").removeClass("submenu-fixed");
+                //console.log('Sidebar hasent passed top of article yet');
                 thisSidebar.css('top', '0');
                 thisSidebar.css('position', 'relative');
-                //thisSidebar.css('bottom', '2.5rem');
                 //Scroll has passed the top of the article - fix the sidebar
             } else {
-                console.log('let the scrolln beign');
-                //jQuery("#sidebar-first").addClass("submenu-fixed");
+                //console.log('let the scrolln beign');
                 thisSidebar.css('top','45.27px');
                 thisSidebar.css('position', 'fixed');
-                //thisSidebar.css('bottom', 'auto');
+                thisSidebar.css('max-width', '433px');
             }
         }
     } else {
     //Window should now be one column
-        console.log('Break the design');
+        //console.log('Break the design');
     }
 }
-
 $(function () {
     positionSidebar();
     $(window).scroll(function () {
         positionSidebar();
     });
 });
-
-
 })(jQuery, Drupal);
