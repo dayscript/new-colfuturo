09fa1a417097ecc3bb6423631fe366d2b49feead
cvillamil <cvillamil@dayscript.com>
2019-09-19 20:14:23 -0500

    block events

--- web/modules/custom/event_brite/src/Plugin/Block/NextEventsBlock.php
+++ web/modules/custom/event_brite/src/Plugin/Block/NextEventsBlock.php
@@ -53,14 +53,23 @@ class NextEventsBlock extends BlockBase {
     $build['default_block_inputtext']['#markup'] = '<p>' . $this->configuration['inputtext'] . '</p>';
     $build['#events']   = $this->getEventsData()->events;  
     $build['#events'] = array_chunk($build['#events'], 3, true);
-    dpm($build['#events']);
+    //dpm($build['#events']);
     return $build;
   }
 
   public function getEventsData(){
-    //$data = file_get_contents('https://www.eventbriteapi.com/v3/users/me/events/?token=BJHRZYF254GXBOV7A6J6');
-    $data = file_get_contents('https://www.eventbriteapi.com/v3/users/me/events/?token=Z5PUBQ6ROR7OYZM7YARV');
+    # Get next 3 events of all programs
+    $data = file_get_contents('https://www.eventbriteapi.com/v3/users/me/events/?order_by=start_asc&page_size=3&time_filter=current_future&token=Z5PUBQ6ROR7OYZM7YARV');
+    
+    # Get events of Programa Crédito Beca id=27056775865
+    $dataPcb = file_get_contents('https://www.eventbriteapi.com/v3/users/me/events/?order_by=start_asc&page_size=1&time_filter=current_future&organizer_filter=27056775865&token=Z5PUBQ6ROR7OYZM7YARV');
+
+    # Get events of Programa de idiomas id=27056802047
+    $dataPi = file_get_contents('https://www.eventbriteapi.com/v3/users/me/events/?order_by=start_asc&page_size=1&time_filter=current_future&organizer_filter=27056802047&token=Z5PUBQ6ROR7OYZM7YARV');
+
+    # Get events of Asesoria id=20071073871
+    $dataAsesoria = file_get_contents('https://www.eventbriteapi.com/v3/users/me/events/?order_by=start_asc&page_size=1&time_filter=current_future&organizer_filter=20071073871&token=Z5PUBQ6ROR7OYZM7YARV');
+
     return json_decode($data);
-  }
-  
+  } 
 }
