233932aa5ef2cc039cb6158802cf29e9a2958bb3
cvillamil <cvillamil@dayscript.com>
2019-09-22 17:35:43 -0500

    shared events

--- web/themes/custom/colfuturo/js/colfuturo.js
+++ web/themes/custom/colfuturo/js/colfuturo.js
@@ -11,6 +11,15 @@
   Drupal.behaviors.exampleBehavior = {
     attach: function (context, settings) {
 
+      $("#events .share a").click(function(){
+        $('.share-social-media').addClass('visible-block');
+      });
+      $(document).on("click", function(event){
+        if(!$(event.target).closest("#events .share a").length){
+          $('.share-social-media').removeClass('visible-block');
+        }
+      });
+
       //MENU #block-homemenu
       var pathArray = window.location.pathname.split('/');
       if (pathArray[1] == 'for-international-universities') {
