2901e946c2f04cb837ff640f925c2b7d60b361be
cvillamil <cvillamil@dayscript.com>
2019-12-05 10:07:26 -0500

    css

---
+++ web/themes/custom/colfuturo/scss/modules/untitled.js
@@ -0,0 +1,32 @@
+jQuery('.slick-carousel .item-list ul').owlCarousel({
+    stagePadding:50,
+    pagination:false,
+    autoplay:true,
+    margin: 10,
+    itemsMobile : [479,1.25],
+})
+
+jQuery('.owl-theme .owl-wrapper-outer .owl-wrapper').owlCarousel({
+    stagePadding:50,
+    pagination:false,
+    autoplay:true,
+    itemsMobile : [479,1.25],
+})
+
+jQuery('.slick-carousel .item-list ul').owlCarousel({
+    loop: true,
+    pagination: false,
+    items: 4,
+    navigation: true,
+    autoplay: true,
+})
+
+beneficiario: ACTUALIZAR EXISTENTE
+potencialprueba: CREARLO?
+
+Cargar todos los usuarios
+Desactivar envio de correos
+
+jQuery('.slick-carousel .item-list ul').owlCarousel({
+	stagePadding: 50,
+}
